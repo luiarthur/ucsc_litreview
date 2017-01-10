fb24a23b67e9e066c8219e193afa9612890b5dce
Arthur Lui <luiarthur@gmail.com>
2017-01-10 14:56:20 -0800

    yes

--- code/model3/purity3.cpp
+++ code/model3/purity3.cpp
@@ -1,187 +1,13 @@
-#include<Rcpp.h>
-#include<functional> // std::function
-#include<ctime>
-#include<map>
+#include "mcmc.h"
 
-using namespace Rcpp;
-
-// Enable C++11 via this plugin (Rcpp 0.10.3 or later)
-// [[Rcpp::plugins("cpp11")]]
-
-
-double logit(double p) {
-  return log(p / (1-p));
-}
-
-double invLogit(double x) {
-  return 1 / (1 + exp(-x));
-}
-
-double metropolis(double curr, std::function<double(double)> ll, std::function<double(double)> lp, double stepSig)
-{
-  const double cand = R::rnorm(curr,stepSig);
-  const double u = R::runif(0,1);
-  double out;
-
-  if (ll(cand) + lp(cand) - ll(curr) - lp(curr) > log(u)) {
-    out = cand;
-  } else {
-    out = curr;
-  }
-
-  return out;
-}
-
-double metLogit(double curr, std::function<double(double)> ll, std::function<double(double)> lp, double stepSig)
-{
-  auto lp_logit = [lp](double logit_p) { // capture lp in []
-    const double p = invLogit(logit_p);
-    const double log_J = -logit_p + 2 * log(p);
-    return lp(p) + log_J;
-  };
-  auto ll_logit = [ll](double logit_p) { return ll(invLogit(logit_p)); };
-  return invLogit(metropolis(logit(curr), ll_logit, lp_logit, stepSig));
-}
-
-
-int wsample_index(double p[], int n) { // GOOD
-  const double p_sum = std::accumulate(p, p+n, 0.0);
-  const double u = R::runif(0,p_sum);
-
-  int i = 0;
-  double cumsum = 0;
-
-  do {
-    cumsum += p[i];
-    i++;
-  } while (cumsum < u);
-
-  return i-1;
-}
-
-double wsample(double x[], double p[], int n) { // GOOD
-  return x[wsample_index(p,n)];
-}
-
-
-// neal algorithm 8
-NumericVector algo8(double alpha, NumericVector t, 
-                    std::function<double(double,int)> lf,
-                    std::function<double(double)> lg0,
-                    std::function<double()> rg0,
-                    std::function<double(double,
-                                         std::function<double(double)>,
-                                         std::function<double(double)>,
-                                         double)> mh,
-                    double cs) {
-  auto f = [lf](double x, int i){ return exp(lf(x,i)); };
-  const int n = t.size();
-  std::vector<double> newT(t.begin(), t.end());
-
-  // create a map of unique t's
-  std::map<double,int> map_t_count;
-  for (int i=0; i<n; i++) {
-    if (map_t_count.find( t[i] ) != map_t_count.end()) 
-    { // if key exists
-      map_t_count[t[i]]++;
-    } else {
-      map_t_count[t[i]] = 1;
-    }
-  }
-
-  // update each element in t
-  for (int i=0; i<n; i++) {
-    map_t_count[newT[i]]--;
-
-    double aux;
-    if (map_t_count[newT[i]] > 0) {
-      aux = rg0();
-    } else {
-      aux = newT[i];
-      map_t_count.erase(newT[i]);
-    }
-
-    double probAux = alpha * f(aux,i);
-
-    const int K = map_t_count.size();
-    double prob[K+1];
-    double unique_t[K+1];
-    
-    prob[0] = probAux;
-    unique_t[0] = aux;
-
-    int k=1;
-    for (auto const& ut : map_t_count) {
-      prob[k] = ut.second * f(ut.first,i);
-      unique_t[k] = ut.first;
-      k++;
-    }
-
-    newT[i] = unique_t[wsample_index(prob,K)];
-    if (map_t_count.find( newT[i] ) != map_t_count.end()) {
-      map_t_count[newT[i]]++;
-    } else {
-      map_t_count[newT[i]] = 1;
-    }
-  }
-
-  // update by cluster
-  std::map<double,std::vector<int>> map_t_idx;
-  for (int i=0; i<n; i++) {
-    if (map_t_idx.find( newT[i] ) != map_t_idx.end()) { // if key exists
-      map_t_idx[newT[i]].push_back(i);
-    } else {
-      map_t_idx[newT[i]] = {i};
-    }
-  }
-  for (auto const& ut : map_t_idx) {
-    auto idx = ut.second;
-    auto ll = [idx,lf](double tj) {
-      double out = 0;
-      for (int i=0; i<idx.size(); i++) { out += lf(tj,idx[i]); }
-      return out;
-    };
-    auto new_tj = mh(ut.first, ll, lg0, cs);
-    for (int i=0; i<idx.size(); i++) {
-      newT[idx[i]] = new_tj;
-    }
-  }
-
-  return NumericVector(newT.begin(), newT.end());
-}
-
-//////////////////////////////////////////////////////////////////
 struct State {
-    NumericVector phi;
-    double sig2;
-    double mu;
-    NumericVector v;
-    NumericVector m;
-    double w2;
+  std::vector<double> v;
+  std::vector<double> phi;
+  std::vector<double> m;
+  double mu;
+  double w2;
+  double sig2;
 }
-
-std::vector<State> gibbs(State init, std::function<S(S)> update, 
-                         int B, int burn, int printevery) {
-
-  std::vector<State> out(B);
-  out[0] = init;
-
-  for (int i=0; i<B+burn; i++) {
-    if (i <= burn) {
-      out[0] = update(out[0]);
-    } else {
-      out[i-burn] = update(out[i-burn-1]);
-    }
-
-    if (printEvery > 0 && (i+1) % printEvery == 0) {
-      Rcout << "\rProgress:  " << i+1 << "/" << B+burn << "\t";
-    }
-  }
-
-  return out;
-}
-
-//////////////////////////////////////////////////////////////////
 
 //[[Rcpp::export]]
 NumericMatrix fit(NumericVector n1, NumericVector N1, 
@@ -199,40 +25,43 @@ NumericMatrix fit(NumericVector n1, NumericVector N1,
                   // gibbs param
                   int B, int burn, int printEvery) {
 
-  const int N = y.size();
+  const int numLoci = n1.size();
 
-  // v
-  NumericMatrix out_v(N,B);
-  out_v(_,0) = NumericVector(N,0.5);
+  auto update = [n1, N1, N0, M, numLoci,
+                 m_phi, s2_phi, 
+                 a_sig, b_sig,
+                 a_mu, b_mu, cs_mu,
+                 a_m, b_m, cs_m,
+                 a_w, b_w,
+                 alpha,cs_v,lg0,rg0](State& s_old, State& s_new) {
 
-  // phi
-  NumericMatrix out_phi(N,B);
-  out_phi(_,0) = NumericVector(N,0.0);
+    auto p = [](double mu, double vs, double ms) {
+      return mu*vs*ms / (2*(1-mu) + mu*ms);
+    };
 
-  // m
-  NumericMatrix out_m(N,B);
-  out_phi(_,0) = NumericVector(N,2.0);
+    auto z = [N1,N0](double mu, double ms, int s) {
+      return 2*N1[s] / (N0[s] * (2*(1-mu) + mu*ms));
+    };
 
-  // mu
-  NumericVector out_mu(B);
-  out_mu(0) = .5;
+    auto ss = [numLoci](double mu, std::vector<double> phi, std::vector<double> m) {
+      double sum = 0;
+      for (int s=0; s<numLoci; s++) {
+        sum += pow(log(z(mu,m[s],s)) - phi[s], 2.0);
+      }
+      return sum;
+    }
 
-  // w2
-  NumericVector out_w2(B);
-  out_w2(0) = 1;
+    // update sig2 (Gibbs-step)
+    s_new = rig(a_sig+numLoci/2, b_sig+ss(s_old.mu,s_old.phi,s_old.m)/2);
 
-  // sig2
-  NumericVector out_sig2(B);
-  out_sig2(0) = 1;
+    // update phi (Gibbs-step)
+    // update mu (Met-logit)
+    // update v (Neal)
+    // update w2 (Gibbs-step)
+    // update m (Met)
 
+  };
 
-  // functions for neal8
-  auto lf = [y,m](double p, int i) {return y[i]*log(p)+(m[i]-y[i])*log(1-p);};
-  auto lg0 = [](double p){return 0.0;};
-  auto rg0 = [](){return R::runif(0,1);};
-
-  // gibbs loop
-  out = gibbs(init, update, B, burn, printEvery)
-
-  return out;
+  // return
+  List ret;
 }
