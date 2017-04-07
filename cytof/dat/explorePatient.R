66ff537479cbd5d8147d9646583f18d12fd6682e
Arthur Lui <luiarthur@gmail.com>
2017-04-07 09:14:22 -0700

    yes

--- cytof/dat/explorePatient.R
+++ cytof/dat/explorePatient.R
@@ -2,20 +2,30 @@ library(rcommon)
 source("myimage.R")
 
 parse_marker <- function(x) {
-  toupper(gsub("[-|.]","",x))
+  x <- toupper(gsub("[-|.]","",x))
+  x <- toupper(gsub("^KIR","",x))
+  x <- toupper(gsub("^X","",x))
+  x
 }
 
 # Analyze patients/005_D93_CLEAN_cutoff.csv (preped by me)
 path <- "cytof_data_lili/cytof_data_lili/patients/005_D93_CLEAN"
 tmp <- read.csv(paste0(path,"_cutoff.csv"),header=FALSE);
-markers <- toupper(levels(tmp[,1]))
+markers <- parse_marker(levels(tmp[,1]))
 cutoff <- tmp[,2]; names(cutoff) <- markers
 cutoff <- cutoff[order(markers)]
+markers <- names(cutoff)
 rm(tmp)
 
 patient <- read.csv(paste0(path,".csv"))
-colnames(patient) <- toupper(colnames(patient))
+colnames(patient) <- parse_marker(colnames(patient))
 patient <- patient[,order(colnames(patient))]
 
 setdiff(markers,colnames(patient))
 setdiff(colnames(patient),markers)
+
+all(colnames(patient) == names(cutoff))
+all(colnames(patient) == markers)
+
+rbind(cutoff,apply(patient,2,quantile))
+
