76d78544dbe4c8c3939ba026d344a081508fe4ac
Arthur Lui <luiarthur@gmail.com>
2017-07-20 13:35:47 -0700

    psi, tau2 now updated as vectors
    Other things to do:
    - specify covariance of proposal in random walk

--- cytof/src/cpp/psi.h
+++ cytof/src/cpp/psi.h
@@ -1,43 +1,88 @@
 // Updating psi_j. See Section 5.2 of manual.
 
-double log_fc_psi(double psi_j, State &state, const Data &y,
-                  const Prior &prior, const int j) {
-
-  double lp = R::dnorm(psi_j, prior.m_psi, sqrt(prior.s2_psi), 1);
+// Multivariate Update Implementation for psi
+void update_psi(State &state, const Data &y, const Prior &prior) {
+  const int J = get_J(y);
   const int K = state.K;
   double mus_jk;
-  double tau_j = sqrt(state.tau2(j));
   double thresh = prior.mus_thresh;
 
-  double ll = 0;
-  for (int k=0; k < K; k++) {
-    mus_jk = state.mus(j,k);
+  auto log_fc = [&](arma::vec psi) {
+    double ll = 0;
 
-    if (state.Z(j,k) == 1 && mus_jk >= thresh) {
-      ll += log_dtnorm(mus_jk, psi_j, tau_j, thresh, false); // lt = false
-    } else if (state.Z(j,k) == 0 && mus_jk < thresh) {
-      ll += log_dtnorm(mus_jk, psi_j, tau_j, thresh, true); // lt = true
-    } else {
-      ll = -INFINITY;
-      Rcout << "This should not happen (psi.h)" << std::endl;
-      break;
+    for (int j=0; j<J; j++) {
+      double tau_j = sqrt(state.tau2(j));
+      double psi_j = psi(j);
+
+      for (int k=0; k < K; k++) {
+        mus_jk = state.mus(j,k);
+
+        if (state.Z(j,k) == 1 && mus_jk >= thresh) {
+          ll += log_dtnorm(mus_jk, psi_j, tau_j, thresh, false); // lt = false
+        } else if (state.Z(j,k) == 0 && mus_jk < thresh) {
+          ll += log_dtnorm(mus_jk, psi_j, tau_j, thresh, true); // lt = true
+        } else {
+          ll = -INFINITY;
+          Rcout << "This should not happen (psi.h)" << std::endl;
+          break;
+        }
+
+      }
     }
 
-  }
+    double lp = 0;
+    for (int j=0; j<J; j++) {
+      lp += R::dnorm(psi(j), prior.m_psi, sqrt(prior.s2_psi), 1);
+    }
 
-  return ll + lp;
+    return ll + lp;
+  };
+
+
+  state.psi = metropolis::mv(state.psi, log_fc, prior.cs_psi(0)*arma::eye<arma::mat>(J,J));
 };
 
-void update_psi(State &state, const Data &y, const Prior &prior) {
+// Univariate Update Implementation: Poor Mixing.
+//
+//double log_fc_psi(double psi_j, State &state, const Data &y,
+//                  const Prior &prior, const int j) {
+//
+//  double lp = R::dnorm(psi_j, prior.m_psi, sqrt(prior.s2_psi), 1);
+//  const int K = state.K;
+//  double mus_jk;
+//  double tau_j = sqrt(state.tau2(j));
+//  double thresh = prior.mus_thresh;
+//
+//  double ll = 0;
+//  for (int k=0; k < K; k++) {
+//    mus_jk = state.mus(j,k);
+//
+//    if (state.Z(j,k) == 1 && mus_jk >= thresh) {
+//      ll += log_dtnorm(mus_jk, psi_j, tau_j, thresh, false); // lt = false
+//    } else if (state.Z(j,k) == 0 && mus_jk < thresh) {
+//      ll += log_dtnorm(mus_jk, psi_j, tau_j, thresh, true); // lt = true
+//    } else {
+//      ll = -INFINITY;
+//      Rcout << "This should not happen (psi.h)" << std::endl;
+//      break;
+//    }
+//
+//  }
+//
+//  return ll + lp;
+//};
+//
+//void update_psi(State &state, const Data &y, const Prior &prior) {
+//
+//  const int J = get_J(y);
+//  for (int j=0; j < J; j++) {
+//
+//    auto log_fc = [&](double psi_j) {
+//      return log_fc_psi(psi_j, state, y, prior, j);
+//    };
+//
+//    state.psi(j) = metropolis::uni(state.psi(j), log_fc, prior.cs_psi[j]);
+//  }
+//
+//};
 
-  const int J = get_J(y);
-  for (int j=0; j < J; j++) {
-
-    auto log_fc = [&](double psi_j) {
-      return log_fc_psi(psi_j, state, y, prior, j);
-    };
-
-    state.psi(j) = metropolis::uni(state.psi(j), log_fc, prior.cs_psi[j]);
-  }
-
-};
--- cytof/src/cpp/tau2.h
+++ cytof/src/cpp/tau2.h
@@ -1,44 +1,88 @@
 // Updating tau^2_j. See Section 5.3 of manual.
 
-double log_fc_log_tau2(double log_tau2_j, State &state, const Data &y,
-                       const Prior &prior, const int j) {
+// Multivariate Update Implementation for tau2
+void update_tau2(State &state, const Data &y, const Prior &prior) {
+  const int J = get_J(y);
   const int K = state.K;
   const double thresh = prior.mus_thresh; // log(2)
   double mus_jk;
-  const double psi_j = state.psi(j);
-  const double tau_j = sqrt(exp(log_tau2_j));
-  const double lp = lp_log_invgamma(log_tau2_j, prior.a_tau, prior.b_tau);
 
-  double ll=0;
-  for (int k=0; k < K; k++) {
-    mus_jk = state.mus(j,k);
-    //ll += log_dtnorm(mus_jk, psi_j, tau_j, thresh, 1-state.Z(j,k)); // lt = false
-    if (state.Z(j,k) == 1 && mus_jk >= thresh) {
-      ll += log_dtnorm(mus_jk, psi_j, tau_j, thresh, false); // lt = false
-    } else if (state.Z(j,k) == 0 && mus_jk < thresh) {
-      ll += log_dtnorm(mus_jk, psi_j, tau_j, thresh, true); // lt = true
-    } else {
-      ll = -INFINITY;
-      Rcout << "This should not happen (tau.h)" << std::endl;
-      break;
+  auto log_fc = [&](arma::vec log_tau2) {
+    double lp = 0;
+    for (int j=0; j<J; j++) {
+      lp += lp_log_invgamma(log_tau2(j), prior.a_tau, prior.b_tau);
     }
 
-  }
+    double ll = 0;
+    for (int j=0; j<J; j++) {
+      double tau_j = sqrt(exp(log_tau2(j)));
+      double psi_j = state.psi(j);
 
-  return ll + lp;
-};
+      for (int k=0; k < K; k++) {
+        mus_jk = state.mus(j,k);
 
-void update_tau2(State &state, const Data &y, const Prior &prior) {
+        if (state.Z(j,k) == 1 && mus_jk >= thresh) {
+          ll += log_dtnorm(mus_jk, psi_j, tau_j, thresh, false); // lt = false
+        } else if (state.Z(j,k) == 0 && mus_jk < thresh) {
+          ll += log_dtnorm(mus_jk, psi_j, tau_j, thresh, true); // lt = true
+        } else {
+          ll = -INFINITY;
+          Rcout << "This should not happen (tau.h)" << std::endl;
+          break;
+        }
 
-  const int J = get_J(y);
-  for (int j=0; j < J; j++) {
+      }
+    }
 
-    auto log_fc = [&](double log_tau2_j) {
-      return log_fc_log_tau2(log_tau2_j, state, y, prior, j);
-    };
+    return ll + lp;
+  };
 
-    state.tau2(j) = exp(metropolis::uni(log(state.tau2(j)), 
-                                        log_fc, prior.cs_tau2[j]));
-  }
+  state.tau2 = exp(metropolis::mv(log(state.tau2), 
+                                  log_fc,  prior.cs_tau2(0)*arma::eye<arma::mat>(J,J) ));
+}
 
-};
+//Univariate Update of tau2. Poor Mixing.
+//
+//double log_fc_log_tau2(double log_tau2_j, State &state, const Data &y,
+//                       const Prior &prior, const int j) {
+//  const int K = state.K;
+//  const double thresh = prior.mus_thresh; // log(2)
+//  double mus_jk;
+//  const double psi_j = state.psi(j);
+//  const double tau_j = sqrt(exp(log_tau2_j));
+//  const double lp = lp_log_invgamma(log_tau2_j, prior.a_tau, prior.b_tau);
+//
+//  double ll=0;
+//  for (int k=0; k < K; k++) {
+//    mus_jk = state.mus(j,k);
+//    //ll += log_dtnorm(mus_jk, psi_j, tau_j, thresh, 1-state.Z(j,k)); // lt = false
+//    if (state.Z(j,k) == 1 && mus_jk >= thresh) {
+//      ll += log_dtnorm(mus_jk, psi_j, tau_j, thresh, false); // lt = false
+//    } else if (state.Z(j,k) == 0 && mus_jk < thresh) {
+//      ll += log_dtnorm(mus_jk, psi_j, tau_j, thresh, true); // lt = true
+//    } else {
+//      ll = -INFINITY;
+//      Rcout << "This should not happen (tau.h)" << std::endl;
+//      break;
+//    }
+//
+//  }
+//
+//  return ll + lp;
+//};
+//
+//void update_tau2(State &state, const Data &y, const Prior &prior) {
+//
+//  const int J = get_J(y);
+//  for (int j=0; j < J; j++) {
+//
+//    auto log_fc = [&](double log_tau2_j) {
+//      return log_fc_log_tau2(log_tau2_j, state, y, prior, j);
+//    };
+//
+//    state.tau2(j) = exp(metropolis::uni(log(state.tau2(j)), 
+//                                        log_fc, prior.cs_tau2[j]));
+//  }
+//
+//};
+
