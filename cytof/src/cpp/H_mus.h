9b56341b21f68e223faba644b6666e4ef4c7fbb2
Arthur Lui <luiarthur@gmail.com>
2017-10-13 13:34:42 -0700

    yes

--- cytof/src/cpp/H_mus.h
+++ cytof/src/cpp/H_mus.h
@@ -1,5 +1,29 @@
 // Updating H. See Section 5.7 of manula
 
+double compute_bk(const State &state, int k) {
+  // k is the index of the array
+
+  double b_k = 1;
+
+  for (int l=0; l<=k; l++) {
+    b_k *= state.v(l);
+  }
+
+  return b_k;
+}
+
+std::vector<double> compute_b(const State &state) {
+  const int K = state.K;
+  std::vector<double> b(K);
+
+  b[0] = state.v[0];
+  for (int l=1; l < K; l++) {
+    b[l] = b[l-1] * state.v[l];
+  }
+
+  return b;
+}
+
 double log_acc_ratio_Hjk_mus_jk(const State &state, const Data & y, 
                                 const Prior &prior, int j, int k,
                                 double mj, double S2j,
@@ -13,6 +37,7 @@ double log_acc_ratio_Hjk_mus_jk(const State &state, const Data & y,
   const double mus_jk = state.mus(j, k);
   const int z_jk = state.Z(j, k);
 
+  // difference of log prior 
   log_r = pow(cand_h_jk - mj, 2) - pow(h_jk - mj, 2) / (-2*S2j);
 
 //#pragma omp parallel for
@@ -35,34 +60,8 @@ double log_acc_ratio_Hjk_mus_jk(const State &state, const Data & y,
 void update_Hjk_mus_jk(State &state, const Data &y, const Prior &prior,
                        int j, int k, double mj, double S2j) {
 
-  //const int I = get_I(y);
-  ////const int J = get_J(y);
-  ////int N_i;
-
-
-  //auto log_fc = [&](double h_jk, double mus_jk, int z_jk) {
-  //  const double lp = R::dnorm(h_jk, mj, sqrt(S2j), 1);
-  //  double ll = 0;
-
-
-////#pragma omp parallel for
-  //  for (int i=0; i<I; i++) {
-  //    //N_i = get_Ni(y, i);
-  //    for (int n=0; n<get_Ni(y,i); n++) {
-  //      if (state.lam[i][n] == k) {
-  //        ll += marginal_lf(y[i](n,j), mus_jk, sqrt(state.sig2(i)),
-  //                          z_jk, state.pi(i,j));
-  //      }
-  //    }
-  //  }
-
-  //  return ll + lp;
-  //};
-
-
   const double h_jk = state.H(j, k);
-  double b_k = 1;
-  for (int l=0; l<=k; l++) { b_k *= state.v(l); }
+  const double b_k = compute_bk(state, k);
   const double cand_h_jk = R::rnorm(h_jk, prior.cs_h);
   const double cand_z_jk = compute_z(cand_h_jk, prior.G(j,j), b_k);
 
@@ -73,9 +72,6 @@ void update_Hjk_mus_jk(State &state, const Data &y, const Prior &prior,
   }
 
   const double u = R::runif(0,1);
-  //if (log_fc(cand_h_jk, cand_mus_jk, cand_z_jk) -
-  //    log_fc(state.H(j, k), state.mus(j,k), state.Z(j,k)) > log(u)) {
-
   const double log_acc_ratio = 
     log_acc_ratio_Hjk_mus_jk(state, y, prior, j, k, mj, S2j,
                              cand_h_jk, cand_z_jk, cand_mus_jk);
@@ -86,6 +82,60 @@ void update_Hjk_mus_jk(State &state, const Data &y, const Prior &prior,
     state.H(j, k) = cand_h_jk;
   }
 }   
+
+void update_Hj_mus_j(State &state, const Data &y, const Prior &prior, int j) {
+  const int K = state.K;
+  const auto h_j = state.H.row(j);
+
+  const auto b = compute_b(state);
+
+  // draw proposed hj
+  arma::Row<double> cand_hj(K);
+  for (int k=0; k<K; k++) {
+    cand_hj[k] = R::rnorm(h_j[k], prior.cs_h/10);
+  }
+
+  // compute proposed zj
+  arma::Row<int> cand_zj(K);
+  for (int k=0; k< K; k++) {
+    cand_zj[k] = compute_z(cand_hj[k], prior.G(j,j), b[k]);
+  }
+
+  arma::Row<double> cand_mus_j = state.mus.row(j);
+  for (int k=0; k<K; k++) {
+    if (cand_zj[k] != state.Z(j,k)) {
+      cand_mus_j[k] = rmus(state.psi(j), sqrt(state.tau2(j)),
+                           cand_zj[k], prior.mus_thresh);
+    }
+  }
+
+  const double u = R::runif(0,1);
+
+  // Compute mj vector
+  std::vector<double> mj(K);
+  const int J = get_J(y);
+  const auto j_idx = arma::regspace<arma::vec>(0, J-1);
+  const arma::uvec minus_j = arma::find(j_idx != j);
+  for (int k=0; k<K; k++) {
+    arma::uvec at_k; at_k << k;
+    // this should be 1 x 1
+    const arma::vec mj_vec = prior.R.row(j) * state.H(minus_j, at_k);
+    mj[k] = mj_vec(0);
+  }
+
+  double log_acc_ratio = 0;
+  for (int k=0; k<K; k++) {
+    log_acc_ratio += 
+      log_acc_ratio_Hjk_mus_jk(state, y, prior, j, k, mj[k], prior.S2(j),
+                               cand_hj[k], cand_zj[k], cand_mus_j[k]);
+  }
+
+  if (log_acc_ratio > log(u)) {
+    state.Z.row(j) = cand_zj;
+    state.mus.row(j) = cand_mus_j;
+    state.H.row(j) = cand_hj;
+  }
+}
 
 void update_H_mus(State &state, const Data &y, const Prior &prior) {
   const int J = get_J(y);
@@ -99,6 +149,9 @@ void update_H_mus(State &state, const Data &y, const Prior &prior) {
       const arma::vec mj = prior.R.row(j) * state.H(minus_j, at_k);
       update_Hjk_mus_jk(state, y, prior, j, k, mj(0), prior.S2(j));
     }
+
+    // update eacj row together (THIS IS NEW STUFF...)
+    update_Hj_mus_j(state, y, prior, j);
   }
 }
 
