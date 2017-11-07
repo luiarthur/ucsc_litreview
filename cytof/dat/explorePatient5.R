d59e83395e08969e129a5414b0863eef2288e365
Arthur Lui <luiarthur@gmail.com>
2017-11-06 22:22:01 -0800

    yes

--- cytof/dat/explorePatient5.R
+++ cytof/dat/explorePatient5.R
@@ -40,24 +40,29 @@ stopifnot(
   all(colnames(pat5_d54) == colnames(pat5_d93))
 )
 
+redToBlue <- colorRampPalette(c('red','grey90','blue'))(12)
+
 # CORRELATION
 my.image(cor(pat5_d54),f=label_markers,xaxt='n',yaxt='n',xlab="",ylab="",
+         mn=-1,mx=1,col=redToBlue,
          main="Patient 5 (Day 54) Correlation b/w Markers",addLegend=TRUE)
 my.image(cor(pat5_d70),f=label_markers,xaxt='n',yaxt='n',xlab="",ylab="",
+         mn=-1,mx=1,col=redToBlue,
          main="Patient 5 (Day 70) Correlation b/w Markers",addLegend=TRUE)
 my.image(cor(pat5_d93),f=label_markers,xaxt='n',yaxt='n',xlab="",ylab="",
+         mn=-1,mx=1,col=redToBlue,
          main="Patient 5 (Day 93) Correlation b/w Markers",addLegend=TRUE)
 
 for (p in seq(.1,.9,by=.1)) 
-my.image(cor(pat5_d54)>p,f=label_markers,xaxt='n',yaxt='n',xlab="",ylab="",
+my.image(abs(cor(pat5_d54))>p,f=label_markers,xaxt='n',yaxt='n',xlab="",ylab="",
          main=paste0("Patient 5 (Day 54) Correlation b/w Markers > ",p*100,"%"))
 
 for (p in seq(.1,.9,by=.1)) 
-my.image(cor(pat5_d70)>p,f=label_markers,xaxt='n',yaxt='n',xlab="",ylab="",
+my.image(abs(cor(pat5_d70))>p,f=label_markers,xaxt='n',yaxt='n',xlab="",ylab="",
          main=paste0("Patient 5 (Day 70) Correlation b/w Markers > ",p*100,"%"))
 
 for (p in seq(.1,.9,by=.1)) 
-my.image(cor(pat5_d93)>p,f=label_markers,xaxt='n',yaxt='n',xlab="",ylab="",
+my.image(abs(cor(pat5_d93))>p,f=label_markers,xaxt='n',yaxt='n',xlab="",ylab="",
          main=paste0("Patient 5 (Day 93) Correlation b/w Markers > ",p*100,"%"))
 
 
@@ -114,13 +119,13 @@ my.image(t(summary_zeros)>p,xlab="",ylab="Markers",xaxt="n",yaxt="n",
 pdf("img/hist.pdf")
 par(mfrow=c(3,1),mar=c(2,4,3,2))
 for (i in 1:ncol(pat5_d54)) {
-  hist(log(1 + pat5_d54[,i] / pat5_d54_cutoff[i]),xlab="",
+  hist(log(1 + pat5_d54[,i] / pat5_d54_cutoff[i]),xlab="",xlim=c(0,6),
        main=paste0("Patient 5 Day 54 -- ", colnames(pat5_d54)[i],": log(1 + expression / cutoff)"),prob=TRUE,ylab="Probability")
 
-  hist(log(1 + pat5_d70[,i] / pat5_d70_cutoff[i]),xlab="",
+  hist(log(1 + pat5_d70[,i] / pat5_d70_cutoff[i]),xlab="",xlim=c(0,6),
        main=paste0("Patient 5 Day 70 -- ", colnames(pat5_d54)[i],": log(1 + expression / cutoff)"),prob=TRUE,ylab="Probability")
 
-  hist(log(1 + pat5_d93[,i] / pat5_d93_cutoff[i]),xlab="",
+  hist(log(1 + pat5_d93[,i] / pat5_d93_cutoff[i]),xlab="",xlim=c(0,6),
        main=paste0("Patient 5 Day 93 -- ", colnames(pat5_d54)[i],": log(1 + expression / cutoff)"),prob=TRUE,ylab="Probability")
   #readline()
 }
@@ -138,3 +143,29 @@ plot.expression.mean.sd(list(pat5_d54,pat5_d70,pat5_d93),
 plot.expression.mean.sd(pbs,pb_cutoff,main="PB: log[(y+eps)/cutoff]")
 plot.expression.mean.sd(cbs,cb_cutoff,main="CB: log[(y+eps)/cutoff]")
 dev.off()
+
+
+### NEW: image of Data
+x <- as.matrix(pat5_d54)
+co <- pat5_d54_cutoff
+dim(x)
+z <- log(t(t(x) / co))
+
+#plot(apply(z,2,function(zc) min(zc[zc > 0])), xlab='', xaxt='n', fg='grey', 
+plot(apply(z,2,function(zc) min(zc[zc > -Inf])), xlab='', xaxt='n', fg='grey', 
+     pch=20, cex=2)
+label_markers(pat5_d54)
+abline(v=1:ncol(z), col='grey85', lty=2)
+
+hist(apply(z,2,function(zc) min(zc[zc > -Inf])))
+
+my.image(z, mn=.03, mx=3, col=redToBlue, f=label_markers,xlab="", xaxt='n',
+         addLegend=T)
+
+plot(colMeans(x), type='h', xlab='', xaxt='n')
+axis(1,at=1:ncol(z), label=colnames(pat5_d54), las=2, cex.axis=.6, fg='grey')
+
+hist(z, border='white', col='steelblue')
+min(z[z > -Inf])
+
+
