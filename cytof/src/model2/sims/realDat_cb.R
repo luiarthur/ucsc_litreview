dda4fac03253ce9ec92693c83242108923f7372d
Arthur Lui <luiarthur@gmail.com>
2017-11-28 00:39:53 -0800

    yes

--- cytof/src/model2/sims/realDat_cb.R
+++ cytof/src/model2/sims/realDat_cb.R
@@ -36,7 +36,7 @@ for (i in 1:I) {
 
 
 truth = list(K=10)
-system.time(out <- cytof_fix_K_fit(dat$y, truth=truth, B=200, burn=80, thin=5))
+system.time(out <- cytof_fix_K_fit(y, truth=truth, B=200, burn=100, thin=5))
 
 ### Loglike
 ll <- sapply(out, function(o) o$ll)
