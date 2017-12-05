99c3b2162e8310eda245fb77d6c603db805b0c9d
Arthur Lui <luiarthur@gmail.com>
2017-12-04 22:36:57 -0800

    yes

--- cytof/src/model2/cytof2/R/plot_beta.R
+++ cytof/src/model2/cytof2/R/plot_beta.R
@@ -35,7 +35,7 @@ p_missing <- function(o, i, j, y) {
   #' @param y A grid of values to compute the probabilities
   #' @export
 
-  J = NCOL(o$missing_y[[1]])
+  J = NROW(o$Z)
 
   b0ij <- matrix(o$beta_0, ncol=J)[i,j]
   b1j <- o$beta_1[j]
