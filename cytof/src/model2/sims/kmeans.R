ce19a71bf3af2005f70cb6ce25e46509109095f8
Arthur Lui <luiarthur@gmail.com>
2018-01-09 21:39:43 -0800

    yes

---
+++ cytof/src/model2/sims/kmeans.R
@@ -0,0 +1,64 @@
+library(cytof2)
+
+load("dat/cytof_cb.RData")
+Y = do.call(rbind, y)
+Y = ifelse(is.na(Y), -10, Y)
+
+my.image(Y, col=blueToRed(), mn=-4, mx=4, xlab='markers', ylab='obs', addL=T)
+
+K = 8
+out = kmeans(Y, K)
+plot(table(out$clus))
+my.image(Y[order(out$clus),],
+         col=blueToRed(), mn=-3, mx=3, xlab='markers', ylab='obs', addL=T)
+
+
+my.image(Y[out$clus==1,], col=blueToRed(), mn=-3, mx=3, addL=T)
+colMeans(Y[out$clus==1,]) > 0
+
+### Z ###
+Z = sapply(1:K, function(k) 1 * (colMeans(Y[out$clus==k,]) > 0))
+my.image(Z)
+
+
+
+
+
+### Simdat $$$
+
+I = 3
+J = 32
+K = 4
+DATA_SIZE = 10000
+USE_SIMPLE_Z = 0
+W <- matrix(c(.3, .4, .2, .1,
+              .1, .7, .1, .1,
+              .2, .3, .3, .2), nrow=I, byrow=TRUE)
+
+dat_lim = c(-10,10)
+y_beta = c(-5,-4.5)
+bdat = get_beta(y=y_beta, p_tar=c(.99,.01), plot=FALSE)
+
+dat <- simdat(I=I, N=c(2,3,1)*DATA_SIZE, J=J, K=K, 
+              b0=matrix(bdat[1],I,J),
+              b1=rep(-bdat[2],J),
+              Z=if (USE_SIMPLE_Z) genSimpleZ(J, K) else genZ(J,K),
+              psi_0=-3, psi_1=3,
+              tau2_0=.1, tau2_1=.1,
+              gams_0=matrix(rnorm(I*J,1,.1),I,J),
+              sig2=matrix(rnorm(I*J,1,.1),I,J),
+              W=W)
+
+Y = do.call(rbind, dat$y)
+Y = ifelse(is.na(Y), -6, Y)
+
+K.hat = 4
+out = kmeans(Y, K.hat)
+plot(table(out$clus))
+my.image(Y[order(out$clus),],
+         col=blueToRed(), mn=-3, mx=3, xlab='markers', ylab='obs', addL=T)
+
+### Z ###
+Z = sapply(1:K.hat, function(k) 1 * (colMeans(Y[out$clus==k,]) > 0))
+my.image(Z[,left_order(Z)])
+my.image(dat$Z)
