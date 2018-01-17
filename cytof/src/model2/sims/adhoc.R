e51523d8e66ebe5f4f3c2bc4af1e9fcfe9ced1b7
Arthur Lui <luiarthur@gmail.com>
2018-01-16 20:35:38 -0800

    yes

---
+++ cytof/src/model2/sims/adhoc.R
@@ -0,0 +1,22 @@
+library(cytof2)
+library(doMC)
+registerDoMC(as.numeric(system("nproc",intern=TRUE)))
+
+adhoc = function(Y, K_min=1, K_max=10, J=5, lam=1, ...) {
+  grid = expand.grid(K_min:K_max, 1:J)
+  N = nrow(grid)
+
+  out = foreach(i=1:N) %dopar% {
+    k = grid[i,1]
+    #km = kmeans((Y>0)*lam, k, ...)
+    #Z = t((km$centers>lam/2) * 1)
+    km = kmeans(Y*lam, k, ...)
+    Z = t((km$centers>0) * 1)
+    Z.unique = unique(Z,MARGIN=2)
+    list(km=km, Z=Z.unique)
+  }
+
+
+  bad_idx = which(sapply(out, function(o) is.na(sum(o$Z))))
+  if (length(bad_idx)) out[-bad_idx] else out
+}
--- cytof/src/model2/sims/kmeans.R
+++ cytof/src/model2/sims/kmeans.R
@@ -1,66 +1,22 @@
 library(cytof2)
+source("adhoc.R")
 
 load("dat/cytof_cb.RData")
 Y = do.call(rbind, y)
-Y = ifelse(is.na(Y), -6, Y)
-
-### COMMENT THIS TO SEE EFFECT ###
-#Y = ifelse(Y < -.5, -3, ifelse(Y < .5, 0, 3))
+Y = ifelse(is.na(Y), -3, Y)
 
 my.image(Y, col=blueToRed(), mn=-4, mx=4, xlab='markers', ylab='obs', addL=T)
+out = adhoc(Y, 10, 15, 8, lam=1, alg='Lloyd', iter.max=500)
 
-K = 10
-out = kmeans(Y, K, alg='Lloyd', iter.max=100)
-plot(table(out$clus))
-my.image(Y[order(out$clus),],
-         col=blueToRed(), mn=-3, mx=3, xlab='markers', ylab='obs', addL=T)
-
-Ymiss = ifelse(Y==-6, NA, Y)
-my.image(Ymiss[out$clus==1,], col=blueToRed(), mn=-3, mx=3, addL=T)
-
-### Image of Y1 by clutsers ###
-my.image(Ymiss[out$clus[1:nrow(y[[1]])]==1,], col=blueToRed(), mn=-3, mx=3, addL=T)
-
-### Z ###
-Z = sapply(1:K, function(k) 1 * (colMeans(Y[out$clus==k,]) > 0))
-Z = unique(Z,MARGIN=2)
-my.image(Z)
-
-
-K = 10; J=5
-out = as.list(1:((K-1) * J))
-km = as.list(1:((K-1) * J))
-i = 1
-for (k in 2:K) {
-  for (j in 1:J) {
-    print(i)
-    tmp = kmeans(Y,k,alg='Lloyd', iter.max=500)
-    #tmp = kmeans((Y>0)*1,k,alg='Lloyd', iter.max=500)
-    #Z = sapply(1:k, function(l) 1 * (colMeans(Y[tmp$clus==l,]) > 0))
-    Z = t((tmp$centers>0) * 1)
-    #Z = t((tmp$centers>.5) * 1)
-    out[[i]] = unique(Z,MARGIN=2)
-    km[[i]] = tmp
-    i = i + 1 
-  }
-}
-
-bad_idx = which(sapply(out, function(x) is.na(sum(x))))
-Z.est = if(length(bad_idx)) estimate_Z(out[-bad_idx]) else estimate_Z(out)
-ind.est = if(length(bad_idx)) estimate_Z(out[-bad_idx],ret=T) else estimate_Z(out, ret=T)
-my.image(Z.est[,left_order(Z.est)])
-my.image(t(Z.est[,left_order(Z.est)]))
+Z = lapply(out, function(o) o$Z)
+Z.est = estimate_Z(Z)
+ind.est = estimate_Z(Z, ret=T)
 
 ### Image of Y by clusters ###
 layout(matrix(c(1,1,1,1,1,1,2,2,2), 3, 3, byrow = TRUE))
-my.image(Y[km[[ind.est]]$clus,], mn=-4, mx=4, col=blueToRed())
-my.image(t(Z.est))
+my.image(unique(Y[order(out[[ind.est]]$km$clus),]>0,MARGIN=1))
+my.image(t(Z.est[, left_order(Z.est)]))
 par(mfrow=c(1,1))
-#my.image(km[[ind.est]]$centers, mn=-4, mx=4, col=blueToRed())
-#my.image(t(Z.est[,left_order(Z.est)]))
-#my.image(km[[ind.est]]$centers>0)
-#par(mfrow=c(1,1))
-#my.image(dat$Z[,left_order(dat$Z)])
 
 ### Simdat $$$
 
