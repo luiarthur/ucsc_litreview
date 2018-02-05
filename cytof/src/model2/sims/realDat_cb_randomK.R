e23473aa28ee7440d9fdac89f6cdeac3c3df162f
Arthur Lui <luiarthur@gmail.com>
2018-02-04 21:26:43 -0800

    yes

--- cytof/src/model2/sims/realDat_cb_randomK.R
+++ cytof/src/model2/sims/realDat_cb_randomK.R
@@ -14,6 +14,9 @@ OUTDIR <- args[3]
 B <- as.integer(args[4])
 BURN <- as.integer(args[5])
 THIN <- as.integer(args[6])
+PROP = as.numeric(args[7])
+WARMUP = as.integer(args[8])
+NCORES = as.integer(args[9])
 dat_lim = c(-5,5)
 
 fileDest = function(name) paste0(OUTDIR, name)
@@ -67,18 +70,20 @@ dev.off()
 init = list(K=MCMC_K)
 #prior = list(cs_v=4, cs_h=3, d_w=1/MCMC_K, a_beta=200000, b_beta=10000,
 #             K_min=1, K_max=10, a_K=2)
-prior = list(cs_v=4, cs_h=3, d_w=1/MCMC_K,
-             a_beta=90, b_beta=30,
-             m_betaBar=-11, s2_betaBar=.1, 
-             s2_beta0=.1,
-             K_min=1, K_max=10, a_K=2)
+
+bdat.prior = get_beta_new(y=c(-3, c0 <- -2, -1), p_tar=c(.1, .6, .01))
+prior = list(cs_v=4, cs_h=3, d_w=1,
+             m_betaBar=bdat.prior['b0'], s2_betaBar=.0001, s2_beta0=.0001, #b0
+             a_beta=bdat.prior['b1'] * 100, b_beta=100, # b1
+             c0=c0, a_x=bdat.prior['x'] * 100, b_x=100, # x
+             K_min=1, K_max=16, a_K=2)
 
 print("Start MCMC")
 
 sim_time <- system.time(
   out <- cytof_fix_K_fit(y, init=init, prior=prior, B=B, burn=BURN, thin=THIN,
-                         warmup=2000, thin_K=5, ncores=8,
-                         prop_for_training=.1, print=1)
+                         warmup=WARMUP, thin_K=1, ncores=NCORES,
+                         prop_for_training=PROP, print=1)
 )
 sink(fileDest('simtime.txt')); print(sim_time); sink()
 save(y, out, file=fileDest('sim_result.RData'))
