dea08271b9737deae29ea201ceaf358a1f740e5e
Arthur Lui <luiarthur@gmail.com>
2018-03-10 15:44:27 -0800

    back to normal likelihood

--- cytof/src/model2/cytof2/src/loglike.h
+++ cytof/src/model2/cytof2/src/loglike.h
@@ -1,7 +1,3 @@
-double dlt(double x, double m, double s, int lg) {
-  return R::dt(x-m, s, lg)
-}
-
 double y_final(const State &state, const Data &y, int i, int n, int j) {
   return missing(y,i,n,j) ? state.missing_y[i](n,j) : y[i](n,j);
 }
@@ -54,10 +50,8 @@ double ll_p_given_beta(const State &state, const Data &y, const Prior &prior,
 
 double ll_f(const State &state, const Data &y, int i, int n, int j) {
   const int lg = 1; // log the density
-  //return R::dnorm(y_final(state, y, i, n, j), mu(state, i, n, j),
-  //                sqrt((1 + gam(state, i, n, j)) * state.sig2(i,j)), lg);
-  return dlt(y_final(state, y, i, n, j), mu(state, i, n, j),
-             sqrt((1 + gam(state, i, n, j)) * state.sig2(i,j)), lg);
+  return R::dnorm(y_final(state, y, i, n, j), mu(state, i, n, j),
+                  sqrt((1 + gam(state, i, n, j)) * state.sig2(i,j)), lg);
 }
 
 double ll_marginal(const State &state, const Data & y, int i, int n, int j) {
@@ -71,12 +65,9 @@ double ll_marginal(const State &state, const Data & y, int i, int n, int j) {
 
   for (int k=0; k<K; k++) {
     g = state.Z(j,k) == 0 ? state.gams_0(i,j) : 0;
-    //fm += state.W(i,k) * R::dnorm(y[i](n,j),
-    //                              state.mus(i,j,state.Z(j,k)),
-    //                              sqrt(state.sig2(i,j) * (1 + g)), lg);
-    fm += state.W(i,k) * dlt(y[i](n,j),
-                             state.mus(i,j,state.Z(j,k)),
-                             sqrt(state.sig2(i,j) * (1 + g)), lg);
+    fm += state.W(i,k) * R::dnorm(y[i](n,j),
+                                  state.mus(i,j,state.Z(j,k)),
+                                  sqrt(state.sig2(i,j) * (1 + g)), lg);
   }
 
   return log(fm);
@@ -102,10 +93,8 @@ double rand_marginal(const State &state, int i, int n, int j) {
 double ll_fz(const State &state, const Data &y, int i, int n, int j, int zz) {
   const double gam_inj = (zz == 0) ? state.gams_0(i,j) : 0;
   const int lg = 1; // log the density
-  //return R::dnorm(y_final(state, y, i, n, j), state.mus(i, j, zz),
-  //                sqrt((1 + gam_inj) * state.sig2(i,j)), lg);
-  return dlt(y_final(state, y, i, n, j), state.mus(i, j, zz),
-             sqrt((1 + gam_inj) * state.sig2(i,j)), lg);
+  return R::dnorm(y_final(state, y, i, n, j), state.mus(i, j, zz),
+                  sqrt((1 + gam_inj) * state.sig2(i,j)), lg);
 }
 
 double loglike(const State &state, const Data &y, const Prior &prior, bool normalize=false) {
