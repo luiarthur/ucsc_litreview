e795a88ed5024678a3f460506afa382abbbdcbb6
Arthur Lui <luiarthur@gmail.com>
2018-04-03 11:58:08 -0700

    removed rcpp helper files

---
+++ cytof/src/model3/cytof3/R/zzz.R
@@ -0,0 +1,12 @@
+#' @useDynLib cytof3
+#' @import rcommon
+#' @import foreach
+#' @import doMC
+#' @importFrom Rcpp evalCpp
+#' @exportPattern "^[[:alpha:]]+"
+#### ORIGINAL NAMESPACE FILE #######
+# useDynLib(cytof3)
+# importFrom(Rcpp, evalCpp)
+# exportPattern("^[[:alpha:]]+")
+####################################
+NULL
