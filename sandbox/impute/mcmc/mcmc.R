41058ea48223e1cf44f078198022622680b3e236
Arthur Lui <luiarthur@gmail.com>
2018-04-04 23:52:48 -0700

    yes

--- sandbox/impute/mcmc/mcmc.R
+++ sandbox/impute/mcmc/mcmc.R
@@ -31,15 +31,19 @@ mh_mv <- function(x, ll, lp, step_size) {
 }
 
 ### Gibbs Sampler (generic) ###
-gibbs <- function(init, update, B, burn, print_every=0) {
-  out <- as.list(1:B)
-  out[[1]] <- init
+gibbs = function(init, update, B, burn, print_every=0) {
+  out = lapply(as.list(1:B), function(b) init)
 
   for (i in 1:(B+burn)) {
-    if (i - burn < 2) {
-      out[[1]] <- update(out[[1]])
+    if (i - burn <= 1) {
+      out[[1]] = update(out[[1]])
+      # Or if C++
+      # update(out[[1]])
     } else {
-      out[[i-burn]] <- update(out[[i-burn-1]])
+      out[[i-burn]] = update(out[[i-burn-1]])
+      # Or if C++
+      # out[[i-burn]] = out[[i-burn-1]]
+      # update(out[[i-burn]])
     }
     if (print_every > 0 && i %% print_every == 0) {
       cat("\rProgress: ", i, " / ", B+burn)
