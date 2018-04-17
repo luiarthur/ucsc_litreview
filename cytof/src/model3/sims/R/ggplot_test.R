497a934a658e63bb820b6637e1de3fdf87d61c1c
Arthur Lui <luiarthur@gmail.com>
2018-04-17 15:17:21 -0700

    yes

--- cytof/src/model3/sims/R/ggplot_test.R
+++ cytof/src/model3/sims/R/ggplot_test.R
@@ -7,17 +7,15 @@ y_orig = readRDS('../data/cytof_cb.rds')
 y = preimpute(y_orig, .01)
 Y = do.call(rbind, y)
 N = sapply(y, NROW)
-idx = unlist(sapply(1:length(y), function(i) rep(i, N[i])))
+idx = unlist(sapply(1:length(y), function(i) rep(as.character(i), N[i])))
 dat = data.frame(markers=Y, idx=idx)
 
 #ggplot(Y, aes(markers[1])) + geom_histogram(aes(y=..density..)) + geom_density()
 #ggplot(dat, aes(markers.2B4, fill=idx)) + geom_histogram(aes(y=..density..))
-grid.arrange(layout_matrix = c(1,2))
-multiplot()
-for (m in colnames(dat)) {
-  m[1] = ggplot(dat, aes(markers.2B4, fill=idx)) + stat_density(geom='line') + xlim(-5,5)
-  m[2] = ggplot(dat, aes(markers.CD16, fill=idx)) + stat_density(geom='line') + xlim(-5,5)
-}
+#for (m in colnames(dat)) {
+#  m[1] = ggplot(dat, aes(markers.2B4, fill=idx)) + stat_density(geom='line') + xlim(-5,5)
+#  m[2] = ggplot(dat, aes(markers.CD16, fill=idx)) + stat_density(geom='line') + xlim(-5,5)
+#}
 
 markers = colnames(dat)[-NCOL(dat)]
 J = 8
@@ -31,3 +29,32 @@ for (j in 1:8) {
 multiplot(plotlist=plotlist, cols=2)
 
 
+
+
+ggplot(dat, aes_string("markers.2B4", fill="idx")) + stat_density(geom='line', alpha=.2, fill=idx)
+  
+ggplot(dat, aes_string("markers.2B4", fill="idx")) + geom_histogram()
+
+
+i = 1
+for (j in 1:8) {
+  yij = y[[i]][,j]
+  marker = colnames(y[[i]])[j]
+  yij_df = data.frame(marker=yij)
+  yij_df$idx = 'data'
+  jit_df = data.frame(marker=yij) + 1
+  jit_df$idx = 'jit'
+  all_dat = rbind(yij_df, jit_df)
+  #m = ggplot(dat, aes(marker, fill=idx))
+  #m = m + stat_density(geom='line') + xlim(-7,8)
+  #m = m + geom_histogram(alpha=.7) + xlim(-7,8)
+  #m = m + stat_density(alpha=.7) + xlim(-7,8)
+  m = ggplot(all_dat, aes(marker, fill=idx))
+  m = m + geom_histogram(alpha=.7) #+ xlim(-7,8)
+  #m = m + stat_density(geom='line') + xlim(-7,8)
+  m
+  ggplot(all_dat, aes(marker, fill=idx)) + geom_density(alpha = 0.2)
+
+  plotlist[[j]] = m
+}
+multiplot(plotlist=plotlist, cols=2)
