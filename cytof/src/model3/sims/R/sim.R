1704eccc5cacf2e4c8da967386657f1543239078
luiarthur <luiarthur@gmail.com>
2018-04-25 12:33:35 -0700

    yes

--- cytof/src/model3/sims/R/sim.R
+++ cytof/src/model3/sims/R/sim.R
@@ -44,6 +44,22 @@ axis(2, at=1:K, fg='grey', las=2, cex.axis=1)
 abline(h=1:K+.5, v=1:J+.5, col='grey')
 dev.off()
 
+pdf(fileDest('Z_true_byW.pdf'))
+for (i in 1:I) {
+  mar = par("mar")
+  par(mar=c(5.1, 4.1, 4.1, 4.1))
+  Wi = dat$W[i,]
+  ord = order(Wi, decreasing=TRUE)
+  my.image(t(dat$Z[,ord]), xlab='markers', ylab='cell-types', xaxt='n', yaxt='n')
+  axis(1, at=1:J, fg='grey', las=2, cex.axis=.8)
+  axis(2, at=1:K, label=ord ,fg='grey', las=2, cex.axis=1)
+  axis(4, at=1:K, paste0(round(Wi[ord]*100, 1),'%') ,fg='grey', las=2, cex.axis=1)
+  abline(h=1:K+.5, v=1:J+.5, col='grey')
+  par(mar=mar)
+}
+dev.off()
+
+
 png(fileDest('Y%03d.png'))
 for (i in 1:I) {
   my.image(y[[i]], col=blueToRed(), addL=TRUE, mn=dat_lim[1], mx=dat_lim[2])
@@ -261,6 +277,25 @@ for (i in 1:I) {
 }
 par(mar=c(5,4,4,2)+.1)
 dev.off()
+
+
+pdf(fileDest('Z_best_byW.pdf'))
+for (i in 1:I) {
+  mar = par("mar")
+  par(mar=c(5.1, 4.1, 4.1, 4.1))
+  Zi = out[[idx_best[i]]]$Z
+  Wi = out[[idx_best[i]]]$W[i,]
+  ord = order(Wi, decreasing=TRUE)
+  my.image(t(Zi[,ord]), xlab='markers', ylab='cell-types', xaxt='n', yaxt='n')
+  axis(1, at=1:J, fg='grey', las=2, cex.axis=.8)
+  axis(2, at=1:length(ord), label=ord ,fg='grey', las=2, cex.axis=1)
+  axis(4, at=1:length(ord), paste0(round(Wi[ord]*100, 1),'%') ,fg='grey', las=2, cex.axis=1)
+  abline(h=1:length(ord)+.5, v=1:J+.5, col='grey')
+  par(mar=mar)
+}
+dev.off()
+
+
 
 Zs = lapply(out, function(o) o$Z)
 idx_best_old = estimate_Z_old(Zs, returnIndex=TRUE)
