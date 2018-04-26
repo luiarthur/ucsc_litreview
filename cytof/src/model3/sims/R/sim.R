9276348b434d81bf2c72d721e959449294214091
Arthur Lui <luiarthur@gmail.com>
2018-04-25 18:35:03 -0700

    yes

--- cytof/src/model3/sims/R/sim.R
+++ cytof/src/model3/sims/R/sim.R
@@ -154,7 +154,7 @@ save.session(file=fileDest('checkpoint.rda'))
 
 ### Start MCMC ###
 st = system.time(
-  out <- fit_cytof_cpp(y, B=1000, burn=1000, prior=prior, locked=locked,
+  out <- fit_cytof_cpp(y, B=2000, burn=1000, prior=prior, locked=locked,
                        init=init, print_freq=1, show_timings=FALSE,
                        normalize_loglike=TRUE, joint_update_freq=0,
                        ncores=NCORES, print_new_line=TRUE)
