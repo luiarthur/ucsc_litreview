ceb306f40e90c3c7a900d14609b9b15b16e20293
Arthur Lui <luiarthur@gmail.com>
2018-04-28 22:52:14 -0700

    yes

--- cytof/src/model3/sims/R/sim.R
+++ cytof/src/model3/sims/R/sim.R
@@ -404,3 +404,19 @@ dev.off()
 
 #my.image(last(out)$Z)
 #round(last(out)$W[i,] * 100, 2)
+
+### Probability of non-expression for missing y
+pz0_missy = matrix(NA, I, J)
+f = function(i,j,b) {
+  lami = out[[b]]$lam[[i]] + 1
+  lami = lami[which(is.na(y[[i]][,j]))]
+  mean(out[[b]]$Z[j,lami] == 0)
+}
+for (i in 1:I) for (j in 1:J) {
+  pz0_missy_ij = sapply(1:B, function(b) f(i,j,b))
+  pz0_missy_ij_mean = mean(pz0_missy_ij)
+  pz0_missy[i,j] = pz0_missy_ij_mean
+}
+sink('pz0_missing_y.txt')
+print(round(t(pz0_missy),2))
+sink()
