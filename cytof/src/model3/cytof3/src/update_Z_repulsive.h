a493a99b9c8242c4eb4524ae620439b57e6a8cba
Arthur Lui <luiarthur@gmail.com>
2018-05-04 14:34:17 -0700

    Implemented update_v_repulsive.

--- cytof/src/model3/cytof3/src/update_Z_repulsive.h
+++ cytof/src/model3/cytof3/src/update_Z_repulsive.h
@@ -1,4 +1,40 @@
 #ifndef UPDATE_Z_REPULSIVE_H
 #define UPDATE_Z_REPULSIVE_H
 
+#include "mcmc.h"
+#include "State.h"
+#include "Data.h"
+#include "Prior.h"
+#include "Locked.h"
+#include <omp.h>
+
+void update_zjk_repulsive(State &state, const Data &data, const Prior &prior, const Locked &locked, int j, int k){
+  // TODO
+}
+
+void update_zk_repulsive(State &state, const Data &data, const Prior &prior, const Locked &locked, int k){
+  // TODO
+}
+
+void update_Z_repulsive(State &state, const Data &data, const Prior &prior, const Locked &locked, bool update_z_by_column=true){
+
+  if(!locked.Z) {
+    const int J = data.J;
+    const int K = prior.K;
+    for (int k=0; k<K; k++) {
+      if (update_z_by_column) {
+        // update z_k a column at a time
+        update_zk_repulsive(state, data, prior, locked, k);
+      } else {
+        // update z_jk one at a time
+        for (int j=0; j<J; j++) {
+          update_zjk_repulsive(state, data, prior, locked, j, k);
+        }
+      }
+    }
+  }
+
+}
+
+
 #endif
--- cytof/src/model3/cytof3/src/update_v_repulsive.h
+++ cytof/src/model3/cytof3/src/update_v_repulsive.h
@@ -1,4 +1,24 @@
 #ifndef UPDATE_V_REPULSIVE_H
 #define UPDATE_V_REPULSIVE_H
 
+#include "mcmc.h"
+#include "State.h"
+#include "Data.h"
+#include "Prior.h"
+#include "Locked.h"
+
+void update_v_repulsive(State &state, const Data &data, const Prior &prior, const Locked &locked){
+  if (!locked.v && !locked.Z) {
+    const int K = prior.K;
+    const int J = data.J;
+    const double alpha = state.alpha;
+    double sum_zk;
+
+    for (int k=0; k<K; k++) {
+      sum_zk = sum(state.Z(_,k));
+      state.v(k) = R::rbeta(sum_zk + alpha/K, J - sum_zk + 1.0);
+    }
+  }
+}
+
 #endif
