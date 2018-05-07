c24e45b7d71359f91520e74f5a8ad0dc440e2fb9
Arthur Lui <luiarthur@gmail.com>
2018-05-07 14:28:41 -0700

    na.color for my.image

--- cytof/src/model3/cytof3/R/myimage.R
+++ cytof/src/model3/cytof3/R/myimage.R
@@ -44,12 +44,13 @@ color.bar <- function(colorVec, mn, mx=-mn, nticks=length(colorVec)-1, digits=1,
 }
 
 
-my.image <- function(Z,col=grey(seq(1,0,len=12)),fg='grey', 
-                     f=function(dat) stopifnot(TRUE),
-                     truncate=TRUE,
-                     rm0Cols=FALSE,
-                     addLegend=FALSE,nticks=11,mn=0,mx=1,...) {
+my.image.old <- function(Z,col=grey(seq(1,0,len=12)),fg='grey', 
+                         f=function(dat) stopifnot(TRUE),
+                         truncate=TRUE,
+                         rm0Cols=FALSE,
+                         addLegend=FALSE,nticks=11,mn=0,mx=1,...) {
   #' @export
+  #' @deprecated
 
   if (rm0Cols) {
     Z <- Z[, which(colSums(Z) > 0)]
@@ -85,4 +86,56 @@ my.image <- function(Z,col=grey(seq(1,0,len=12)),fg='grey',
 
 }
 
+my.image <- function(Z, col=grey(seq(1,0,len=12)), na.color='transparent',
+                     fg='grey', f=function(dat) stopifnot(TRUE),
+                     truncate=TRUE,
+                     rm0Cols=FALSE,
+                     addLegend=FALSE,nticks=11,zlim=0:1,...) {
+  #' @export
+
+  if (rm0Cols) {
+    Z <- Z[, which(colSums(Z) > 0)]
+  }
+  mn=zlim[1]
+  mx=zlim[2]
+  zstep=(mx - mn) / length(col)
+
+  N = NROW(Z)
+  K = NCOL(Z)
+  COL = col
+  FG = fg
+  NTICKS = nticks
+  #scaledZ <- (Z-min(Z))/ diff(range(Z))
+  #image(1:K, 1:N, 1-t(scaledZ[N:1,]), fg='grey',...)
+
+  if (addLegend) {
+    layout(matrix(c(rep(1,9*10),rep(2,1*10)),10,10))
+  }
+
+  #image(1:K, 1:N, max(Z)-t(Z), fg=FG,col=COL, ...)
+  #image(1:K, 1:N, mx-t(Z), fg=FG,col=COL, ...)
+
+
+  if (truncate) {
+    Z[which(Z >= mx)] = mx
+    Z[which(Z <= mn)] = mn
+    Z[which(is.na(Z))] = mx + zstep
+  }
+  image(1:K, 1:N, t(Z), fg=FG,col=c(COL,na.color), zlim=c(mn, mx + zstep), ...)
+  f(Z)
+
+  if (addLegend) {
+    par(mar=c(5.1,1,4.1,1))
+    color.bar(COL[-length(COL)],mn,mx,nticks)
+    par(mfrow=c(1,1),mar=c(5.1,4.1,4.1,2.1))
+  }
+
+}
+### TEST ###
+#library(cytof3)
+#y = readRDS('sims/data/cytof_cb.rds')
+#ys = y[[1]][1:100,]
+#na.color = 'black'
+#my.image(ys, zlim=c(-3,3), col=blueToRed(), addL=TRUE, na.col='black')
+#my.image.old(ys, mn=-3, mx=3, col=blueToRed(), addL=TRUE)
 
