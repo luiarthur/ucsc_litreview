2ae929e8c16247a481b6bd205060ed02f268abac
Arthur Lui <luiarthur@gmail.com>
2018-05-10 17:09:14 -0700

    yes

---
+++ sandbox/omp/rng/R/parRng/R/zzz.R
@@ -0,0 +1,9 @@
+#' @useDynLib parRng
+#' @importFrom Rcpp evalCpp
+#' @exportPattern "^[[:alpha:]]+"
+#### ORIGINAL NAMESPACE FILE #######
+# useDynLib(cytof3)
+# importFrom(Rcpp, evalCpp)
+# exportPattern("^[[:alpha:]]+")
+####################################
+NULL
