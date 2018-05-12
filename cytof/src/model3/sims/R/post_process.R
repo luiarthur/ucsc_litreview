58fc646ca0d7ecc2903b62e00096bd464697d396
Arthur Lui <luiarthur@gmail.com>
2018-05-11 19:28:00 -0700

    yes

--- cytof/src/model3/sims/R/post_process.R
+++ cytof/src/model3/sims/R/post_process.R
@@ -28,3 +28,20 @@ for (i in 1:I) for (j in 1:J) {
 sink('pz0_missing_y.txt')
 print(round(t(pz0_missy),2))
 sink()
+
+### Principal Components to visualize clusters ###
+lamToChar = function(x) strsplit(rawToChar(as.raw(x + 65)), '')[[1]]
+
+py = lapply(last(out)$missing_y, prcomp)
+idx_best = sapply(1:I, function(i) estimate_ZWi_index(out, i))
+i = 1
+Wi = out[[idx_best[i]]]$W[i,]
+bigK = which(Wi > .05)
+lami = out[[idx_best[i]]]$lam[[i]]
+plot(py[[i]]$x[which(lami %in% bigK),1:2], 
+     col=lami[which(lami %in% bigK)],
+     pch=lamToChar(lami[which(lami %in% bigK)]), cex=.7)
+#plot(py[[i]]$x[,1:2], col=rgb(0,0,1,.3), pch=lamToChar(lam[[i]]), cex=.7)
+#table(lamToChar(lam[[i]])) / sum(table(lamToChar(lam[[i]])))
+#round(out[[idx_best[i]]]$W[i,], 4)
+
