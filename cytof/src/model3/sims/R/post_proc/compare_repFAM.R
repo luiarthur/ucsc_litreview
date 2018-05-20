a7001303a0d8d014d0299d7bd2513d75c162f548
Arthur Lui <luiarthur@gmail.com>
2018-05-19 20:55:13 -0700

    v12 of sampling.tex

--- cytof/src/model3/sims/R/post_proc/compare_repFAM.R
+++ cytof/src/model3/sims/R/post_proc/compare_repFAM.R
@@ -1,29 +1,42 @@
 #!/usr/bin/env Rscript
 library(rcommon)
 library(cytof3)
-library(FlowSOM)
 set.seed(1)
 
 OUTDIR='./'
 fileDest = function(filename) paste0(OUTDIR, filename)
+repeatedCols = function(Z) {
+  NCOL(Z) - uniqueCols(Z)
+}
+repeatedPosCols = function(Z) {
+  nPosCols = length(which(colSums(Z) > 0))
+  nPosCols - uniquePosCols(Z)
+}
+repeats = function(Z) repeatedCols(Z) > 0
+repeatsPos = function(Z) repeatedPosCols(Z) > 0
 
 # repFAM model
 load('../../out/sim_locked_beta_K20_N500_repFam-TRUE/checkpoint.rda')
-best_idx.rfam = sapply(1:I, function(i) estimate_ZWi_index(out, i))
-best_lam.rfam = sapply(1:I, function(i) out[[best_idx.rfam[i]]]$lam[[i]])
+Z.rfam = lapply(out, function(o) o$Z)
+nRepeatedCols.rfam = sapply(Z.rfam, repeatedCols)
+nRepeatedPosCols.rfam = sapply(Z.rfam, repeatedPosCols)
 
 # FAM model
 load('../../out/sim_locked_beta_K20_N500_repFam-FALSE/checkpoint.rda')
-best_idx.fam = sapply(1:I, function(i) estimate_ZWi_index(out, i))
-best_lam.fam = sapply(1:I, function(i) out[[best_idx.fam[i]]]$lam[[i]])
+Z.fam = lapply(out, function(o) o$Z)
+nRepeatedCols.fam = sapply(Z.fam, repeatedCols)
+nRepeatedPosCols.fam = sapply(Z.fam, repeatedPosCols)
 
-# True clusters
-true_lam = dat$lam
 
-# FMeasure
-FM = c("rfam"=FMeasure(unlist(true_lam), unlist(best_lam.rfam)),
-       "fam"=FMeasure(unlist(true_lam), unlist(best_lam.fam)))
-print(FM)
-# F score:
-#      rfam       fam
-# 1.0000000 0.9319114
+# Plots
+par(mfrow=c(1,2))
+plot(table(nRepeatedCols.fam) / sum(table(nRepeatedCols.fam)), ylim=0:1,
+     ylab='proportion')
+plot(table(nRepeatedPosCols.fam) / sum(table(nRepeatedPosCols.fam)), ylim=0:1,
+     ylab='proportoin')
+par(mfrow=c(1,1))
+
+table(nRepeatedPosCols.fam) / sum(table(nRepeatedPosCols.fam))
+
+mean(sapply(Z.fam, repeats))
+mean(sapply(Z.fam, repeatsPos))
---
+++ cytof/src/model3/sims/R/uniquePosCols.R
@@ -0,0 +1,18 @@
+uniquePosCols = function(Z) {
+  #' Find the unique non-zero columns in Z
+  #' @export 
+
+  col0 = which(colSums(Z) == 0)
+  if (length(col0) > 0) {
+    Z = Z[,-col0]
+  }
+
+  NCOL(unique(Z, MARGIN=2))
+}
+
+uniqueCols = function(Z) {
+  #' Find the unique non-zero columns in Z
+  #' @export 
+
+  NCOL(unique(Z, MARGIN=2))
+}
