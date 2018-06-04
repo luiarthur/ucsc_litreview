db724862df4434024000550ceabb8a0c26545f6c
Arthur Lui <luiarthur@gmail.com>
2018-06-04 09:38:20 -0700

    yes

--- cytof/src/model3/sims/R/sim.R
+++ cytof/src/model3/sims/R/sim.R
@@ -506,7 +506,7 @@ dev.off()
 #  #yZ_inspect_old(out, y, dat_lim=dat_lim, i=i, thresh=.05)
 #}
 #dev.off()
-mult=1; png(fileDest('YZ%03d.png'), height=600*mult, width=500*mult, type='Xlib')
+mult=1; png(fileDest('YZ%03d.png'), height=600*mult, width=500*mult)
             #family=X11Fonts()$Arial)
 for (i in 1:I) {
   yZ_inspect(out, y, zlim=dat_lim, i=i, thresh=.9, na.color='black',
