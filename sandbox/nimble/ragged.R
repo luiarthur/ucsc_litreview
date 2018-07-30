74ea377b2de2d71683ceb1390e249c28af8caa0b
Arthur Lui <luiarthur@gmail.com>
2018-07-30 08:44:37 -0700

    added alternative way to fit ragged arrays

--- sandbox/nimble/ragged.R
+++ sandbox/nimble/ragged.R
@@ -2,11 +2,20 @@ library(nimble)
 #library(rcommon)
 set.seed(1)
 
+pad_zeros = function(y_list) {
+  n_max = max(sapply(y_list, length))
+  y_padded = lapply(y_list, function(y) if (length(y) == n_max) y else c(y, double(n_max-length(y))))
+  Reduce(cbind, y_padded)
+}
+y_ls = list(rep(1,3), rep(2,5), rep(3,2))
+pad_zeros(y_ls)
+
 ### Nimble Model Code ###
 model.code = nimbleCode({
   for (i in 1:I) {
     for (j in 1:J[i]) {
-      y[idxOffset[i] + j] ~ dnorm(mu[i], 1)
+      #y[idxOffset[i] + j] ~ dnorm(mu[i], 1) # version 1
+      y[i,j] ~ dnorm(mu[i], 1) # version 2
     }
     mu[i] ~ dnorm(m, var=s2)
   }
@@ -17,7 +26,9 @@ J = c(30, 15, 20)
 mu_true = c(-3, 5, 10)
 y_orig = sapply(1:length(J), function(i) rnorm(J[i], mu_true[i]))
 I = length(y_orig)
-y = Reduce(c, y_orig)
+#y = Reduce(c, y_orig) # version 1
+y = t(pad_zeros(y_orig)) # version 2
+
 hist(y)
 
 ### Model data, constants, and inits
