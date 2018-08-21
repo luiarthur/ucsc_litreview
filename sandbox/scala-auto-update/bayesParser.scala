da79c09b453b397ba400652a8323ce0f82e9de4b
Arthur Lui <luiarthur@gmail.com>
2018-08-21 09:01:33 -0700

    implement parser combinator?

---
+++ sandbox/scala-auto-update/bayesParser.scala
@@ -0,0 +1,47 @@
+/* Version 1: Using Traits...
+trait Constants {
+  var N:Int
+  var J:Int
+  var m:Double = 0
+  var s2:Double = 1.0
+  val alpha:Array[Double] = Array.ofDim[Double](J)
+}
+
+trait Data extends Constants {
+  val y = Array.ofDim[Double](N)
+}
+
+trait Params extends Constants {
+  val c = Array.ofDim[Int](N)
+  val mu = Array.ofDim[Double](J)
+  var sig2:Double = 1
+  val w = Array.ofDim[Double](J) 
+}
+
+case class model(var N:Int, var J:Int) extends Data with Params {
+}
+
+val b = model(10, 3)
+*/
+
+// http://www.lihaoyi.com/post/EasyParsingwithParserCombinators.html
+// Version 2: Parser combinators. Probably go with this...
+import scala.util.parsing.combinators._
+val model = """
+Constants {
+  N: Int
+  J: Int
+  m: Double
+  s2:Double
+  alpha: Array[Double]
+}
+
+Data {
+}
+"""
+
+
+/* Version 3. Reflection...
+import scala.reflect.runtime.universe._
+typeOf[Params].members.toList
+*/
