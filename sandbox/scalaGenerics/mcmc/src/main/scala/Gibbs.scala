0d7fbf3f2bd432772599a4c21707f9b2b9166a33
Arthur Lui <luiarthur@gmail.com>
2018-08-31 06:28:25 -0700

    added a test / example

--- sandbox/scalaGenerics/mcmc/src/main/scala/Gibbs.scala
+++ sandbox/scalaGenerics/mcmc/src/main/scala/Gibbs.scala
@@ -11,29 +11,29 @@ trait Gibbs extends MCMC {
   // Substate to monitor. By default, it is the entire state.
   // thin period is 1 by default
   type Substate1 = State
-  def deepcopy1(m:State): Substate1
+  def deepcopy1(s:State): Substate1 = deepcopy(s)
   val thin1:Int = 1
 
 
   // Optional substates to monitor.
   // thin period is 0 by default. i.e. no thinning.
   type Substate2
-  def deepcopy2(m:State): Substate2
+  def deepcopy2(s:State): Option[Substate2] = None
   val thin2:Int = 0
 
   type Substate3
-  def deepcopy3(m:State): Substate3
+  def deepcopy3(s:State): Option[Substate3] = None
   val thin3:Int = 0
 
 
   // Update function
   def update(s: State): Unit
 
-  type Output = (List[Substate1], List[Substate2], List[Substate3])
+  type Output = (List[Substate1], List[Option[Substate2]], List[Option[Substate3]])
 
   def gibbs(s: State, 
             niter:Int, nburn:Int, printProgress:Boolean=true,
-            _out: Output): Output = {
+            _out: Output = (List(), List(), List())): Output = {
     // Update the current state
     update(s)
 
@@ -47,11 +47,14 @@ trait Gibbs extends MCMC {
       gibbs(s, niter, nburn - 1, printProgress, _out)
     } else if (niter > 0) {
       lazy val newOut1 = 
-        if (niter % thin1 == 0) deepcopy1(s) :: _out._1 else _out._1
+        if (niter % thin1 == 0)
+          deepcopy1(s) :: _out._1 else _out._1
       lazy val newOut2 =
-        if (niter % thin2 == 0) deepcopy2(s) :: _out._2 else _out._2
+        if (thin2 > 0 && niter % thin2 == 0)
+          deepcopy2(s) :: _out._2 else _out._2
       lazy val newOut3 =
-        if (niter % thin3 == 0) deepcopy3(s) :: _out._3 else _out._3
+        if (thin3 > 0 && niter % thin3 == 0)
+          deepcopy3(s) :: _out._3 else _out._3
 
       lazy val newOut = (newOut1, newOut2, newOut3)
       gibbs(s, niter - 1, 0, printProgress, newOut)
--- sandbox/scalaGenerics/mcmc/src/test/scala/TestSuite.scala
+++ sandbox/scalaGenerics/mcmc/src/test/scala/TestSuite.scala
@@ -1,3 +1,44 @@
 import org.scalatest.FunSuite
 class TestSuite extends FunSuite {
+  val printDebug = false
+
+  test("Gibbs") {
+    case class Param(var x: Int, val y: Array[Int])
+    case class Sub2(var x: Int)
+
+    object TestGibbs extends mcmc.Gibbs {
+      type State = Param 
+      def deepcopy(s:State) = s.copy(s.x+0, s.y.clone)
+
+      type Substate2 = Sub2
+      override def deepcopy2(s:State) = Some(Sub2(s.x+0))
+      override val thin2 = 2
+
+      def update(s:State) {
+        s.x += 1
+        s.y(0) -= 1
+      }
+    }
+
+
+    val state = Param(0, Array(0))
+    val (niter, nburn) = (10, 5)
+    val out = TestGibbs.gibbs(state, niter=niter, nburn=nburn, printProgress=true)
+    if (printDebug) println(out)
+    assert(out._1.head.x == niter+nburn && out._1.last.x == 1 + nburn)
+    assert(out._1.head.y(0) == -(niter+nburn) && out._1.last.y(0) == -(1 + nburn))
+
+    out._2.last match {
+      case Some(Sub2(x)) => assert(x == nburn + 1)
+      case _ => assert(false)
+    }
+
+    out._2.head match {
+      case Some(Sub2(x)) => 
+        assert(x == nburn + (if (nburn % 2 == 0) niter else niter-1))
+      case _ => assert(false)
+    }
+
+    assert(out._3 == List())
+  }
 }
